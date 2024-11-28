8b460180f5b7e202a988b0015359282595553b4c
Dennis Keil <dennis.keil10@gmail.com>
2024-11-29 00:43:37 +0100

    cleanup

--- apps/web/app/(auth)/auth/confirm/route.ts
+++ apps/web/app/(auth)/auth/confirm/route.ts
@@ -1,33 +1,7 @@
 import { createClient } from '@/helper/supabase/server'
-import { NextResponse } from "next/server";
-
 import { redirect } from 'next/navigation'
 import { type EmailOtpType } from '@supabase/supabase-js'
 import { type NextRequest } from 'next/server'
-
-// export async function GET(request: Request) {
-//     // The `/auth/callback` route is required for the server-side auth flow implemented
-//     // by the SSR package. It exchanges an auth code for the user's session.
-//     // https://supabase.com/docs/guides/auth/server-side/nextjs
-//     const requestUrl = new URL(request.url);
-//     const code = requestUrl.searchParams.get("code");
-//     const origin = requestUrl.origin;
-//     const redirectTo = requestUrl.searchParams.get("redirect_to")?.toString();
-//
-//     if (code) {
-//         const supabase = await createClient();
-//         await supabase.auth.exchangeCodeForSession(code);
-//     }
-//
-//     if (redirectTo) {
-//         return NextResponse.redirect(`${origin}${redirectTo}`);
-//     }
-//
-//     // URL to redirect to after sign up process completes
-//     return NextResponse.redirect(`${origin}/account`);
-// }
-
-
 
 // This is used by email verification in username/password sign up
 export async function GET(request: NextRequest) {
@@ -52,3 +26,27 @@ export async function GET(request: NextRequest) {
     // redirect the user to an error page with some instructions
     redirect('/error')
 }
+
+// This is for magic link sign in
+// export async function GET(request: NextRequest) {
+//     const { searchParams } = new URL(request.url)
+//     const token_hash = searchParams.get('token_hash')
+//     const type = searchParams.get('type') as EmailOtpType | null
+//     const next = searchParams.get('next') ?? '/'
+//
+//     if (token_hash && type) {
+//         const supabase = await createClient()
+//
+//         const { error } = await supabase.auth.verifyOtp({
+//             type,
+//             token_hash,
+//         })
+//         if (!error) {
+//             // redirect user to specified redirect URL or root of app
+//             redirect(next)
+//         }
+//     }
+//
+//     // redirect the user to an error page with some instructions
+//     redirect('/error')
+// }
--- apps/web/app/(auth)/auth/link/steam/route.ts
+++ apps/web/app/(auth)/auth/link/steam/route.ts
@@ -14,11 +14,9 @@ export async function GET(request: Request) {
     }
 
     const requestUrl = new URL(request.url);
-    // const code = requestUrl.searchParams.get("code");
     const origin = requestUrl.origin;
 
     console.log('auth/link/steam');
-    // console.log('code', code);
     console.log('origin', origin);
     console.log('request.url', request.url);
 
@@ -26,68 +24,6 @@ export async function GET(request: Request) {
     // let signIn = new SteamSignIn('https://aoe2companion.com');
     let steamId = await signIn.verifyLogin(request.url);
     console.log(`User successfully authenticated as ${steamId.getSteamID64()}`);
-
-    // if (code) {
-    //     const body = {
-    //         client_id: process.env.NEXT_PUBLIC_PATREON_CLIENT_ID!,
-    //         client_secret: process.env.NEXT_PUBLIC_PATREON_CLIENT_SECRET!,
-    //         code,
-    //         grant_type: "authorization_code",
-    //         redirect_uri: `${origin}/auth/link`,
-    //     };
-    //     const init = {
-    //         body: new URLSearchParams(Object.entries(body)).toString(),
-    //         method: "POST",
-    //         headers: {
-    //             "content-type": "application/x-www-form-urlencoded",
-    //             accept: "application/json",
-    //         },
-    //     };
-    //     const response = await fetch("https://www.patreon.com/api/oauth2/token", init);
-    //     const results = await response.json();
-    //     if (results.error) {
-    //         return new NextResponse("Patreon Error: " + (results.error_description || results.error), {
-    //             status: 500,
-    //         });
-    //     }
-    //
-    //     console.log('results', results);
-    //
-    //     // const text = 'INSERT INTO users(user_id, access, refresh) VALUES($1, $2, $3)'
-    //     // const values = [user.id, results.access_token, results.refresh_token]
-    //     //
-    //     // const res = await client.query(text, values)
-    //     // console.log(res)
-    //
-    //     const ptResponse = await fetch("https://www.patreon.com/api/oauth2/v2/identity?include=memberships.campaign&fields%5Bmember%5D=patron_status", {
-    //         method: "GET",
-    //         headers: {
-    //             accept: "application/json",
-    //             Authorization: `Bearer ${results.access_token}`,
-    //         },
-    //     });
-    //
-    //     if (ptResponse.status != 200) {
-    //         return ptResponse;
-    //     }
-    //
-    //     try {
-    //         const patreonCampaignId = '13192796';
-    //         const ptData = await ptResponse.json();
-    //         console.log('ptData', JSON.stringify(ptData, null, 2));
-    //         const sponsorships = ptData?.included
-    //             ?.filter(e => e.type === 'member')
-    //             ?.filter(e => e.attributes.patron_status === "active_patron")
-    //             ?.filter(e => e.relationships.campaign.data.id === patreonCampaignId)
-    //         results.patreonOk = sponsorships.length > 0;
-    //         console.log('patreonOk', results.patreonOk);
-    //     } catch {
-    //     }
-    // }
-    //
-    // if (redirectTo) {
-    //     return NextResponse.redirect(`${origin}${redirectTo}`);
-    // }
 
     // URL to redirect to after sign up process completes
     return NextResponse.redirect(`${origin}/account`);
